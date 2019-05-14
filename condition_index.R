55c3a8d17f59ac812f740e9940e6c6598d0db14f
Javier Atalah <jatalah@gmail.com>
2019-05-14 13:19:02 +0200

    updated data

--- condition_index.R
+++ condition_index.R
@@ -1,16 +1,15 @@
 library(tidyverse)
 library(readxl)
 
-ci <- read_excel('CI_index_mussels.xlsx')
-names(ci)
+ci <- read_excel('data/Length and CI.xlsx')
 dotchart(ci$CI)
 
-# two outlier
-head(arrange(ci, desc(CI)))
-# sample 42 and 96 >1,5000
+# remove one outlier
+head(arrange(ci, CI))
+# sample 32 CI<1
 
 ci <- 
-  filter(ci,CI<1000 & CI>3) %>% 
+  filter(ci,CI>1) %>% 
   mutate(Site = fct_recode(Site, Koutou = "K", Shipwreck = "S"),
          Density = fct_recode(Density, Control = "C", Low = "L", High = "H"),
          Density = fct_relevel(Density, "Control", "Low"),
@@ -26,11 +25,11 @@ ggplot(ci, aes(x = Density, y = CI)) +
   theme_bw()
 
 # useless dynamite plot---
-ggplot(ci, aes(x = Density, y = CI)) +  
-  stat_summary(fun.y = mean, geom = "bar", fill = 'gray80', color = 1, alpha =0) +  
-  stat_summary(fun.data =  "mean_cl_boot", geom = "errorbar", width = 0.2) +
-  facet_grid( Type ~ Site) +
-  theme_bw()
+# ggplot(ci, aes(x = Density, y = CI)) +  
+#   stat_summary(fun.y = mean, geom = "bar", fill = 'gray80', color = 1, alpha =0) +  
+#   stat_summary(fun.data =  "mean_cl_boot", geom = "errorbar", width = 0.2) +
+#   facet_grid( Type ~ Site) +
+#   theme_bw()
 
 ggplot(ci, aes(x = Density, y = CI)) +  
   stat_summary(fun.data = "mean_cl_boot", colour = 1, size = 1 ) +
@@ -40,13 +39,20 @@ ggplot(ci, aes(x = Density, y = CI)) +
 und_ci <- filter(ci,Type=="U")
 
 
-ggplot(filter(ci,Type=="U"), aes(x = Density, y = CI)) +  
+ggplot(und_ci, aes(x = Density, y = CI)) +  
   stat_summary(fun.data = "mean_cl_boot", colour = 1, size = 1 ) +
   facet_wrap( ~ Site) +
   theme_bw() +
   ylab('Condition index')
 
-m1 <- aov(CI~Site*Density, und_ci)
-summary(m1)
-plot(m1)
 
+ggplot(und_ci, aes(x = Density, y = CI)) +  
+  geom_boxplot() +
+  facet_wrap( ~ Site) +
+  theme_bw() +
+  ylab('Condition index')
+
+
+m1 <- aov(sqrt(CI)~Site*Density, und_ci)
+anova(m1)
+TukeyHSD(m1)
